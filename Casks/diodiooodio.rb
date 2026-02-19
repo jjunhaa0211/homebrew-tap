ac214b77fe348d3e854d6878145fd80d87ebdd67
jjunhaa0211 <goodjunha@gmail.com>
2026-02-19 21:26:30 +0900

    diodiooodio 1.0.1

--- Casks/diodiooodio.rb
+++ Casks/diodiooodio.rb
@@ -1,6 +1,6 @@
 cask "diodiooodio" do
-  version "1.0.0"
-  sha256 "4aa7e14a153bafd1ed62e9d2a23084e63a4db58fdc225a25e8c6627779fb8a1a"
+  version "1.0.1"
+  sha256 "09e204834766322547f4a47640beb42940df5ccad0f49e35ee411ba20ed2c12f"
 
   url "https://github.com/jjunhaa0211/diodiooodio/releases/download/v#{version}/diodiooodio-#{version}.zip"
   name "diodiooodio"
