b86bc849b634fa1e3798fda4d3b0bf63be040f11
jjunhaa0211 <goodjunha@gmail.com>
2026-03-24 15:03:24 +0900

    Update MyWorkStudio to v1.5.5

--- Casks/myworkstudio.rb
+++ Casks/myworkstudio.rb
@@ -1,6 +1,6 @@
 cask "myworkstudio" do
-  version "1.5.1"
-  sha256 "2aa7ff47561b93360ff0f10ab1962d855c96afffddc552cf92e0f55e66a7e8e2"
+  version "1.5.5"
+  sha256 "8215d4c31d92f2435d8928e712e8e1513ae26b808712122d3736a4a5aeaafd80"
 
   url "https://github.com/jjunhaa0211/MyWorkStudio/releases/download/v#{version}/MyWorkStudio-v#{version}.zip"
   name "MyWorkStudio"
